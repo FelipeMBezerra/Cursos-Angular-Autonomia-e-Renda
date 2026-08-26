289f748176729ee0f1fa501118780c4ad12727c2
Felipe Machado <sonfelipebezerra@gmail.com>
2026-08-26 16:31:39 -0300

    sla

--- src/app/features/cheakout/cheakout/cheakout.ts
+++ src/app/features/cheakout/cheakout/cheakout.ts
@@ -1,9 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+  ValidationErrors
+} from '@angular/forms';
+
+import { CarrinhoService } from '../../../core/service/carrinho.service';
+import { email } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-cheakout',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './cheakout.html',
   styleUrl: './cheakout.css',
 })
-export class Cheakout {}
+export class Cheakout {
+
+  carrinhoService = inject(CarrinhoService);
+
+  formulario = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(3), nomeSemNumeros]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    endereco: new FormControl('', [Validators.required, Validators.minLength(5)]),
+  });
+
+
+  finalizar() {
+
+    if(this.formulario.invalid){
+      console.log('Formulário inválido')
+      return;
+    }
+
+    const dados = this.formulario.value;
+    const itens = this.carrinhoService.itens();
+
+    console.log('Dados do formulário:', dados);
+    console.log('itens do carrinho:', itens);
+  }
+
+
+
+}
+function nomeSemNumeros(control: AbstractControl): ValidationErrors | null {
+
+  const valor = control.value;
+
+  if (!valor) return null;
+
+  if (/\d/.test(valor)) {
+    return { numeroInvalido: true };
+  }
+
+  return null;
+
+}
